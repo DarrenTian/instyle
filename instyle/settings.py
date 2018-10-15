cb69634e90da4de5cb06c78b7075d09ea5b77763
Darren Tian <darrentianyy@gmail.com>
2018-10-14 20:04:02 -0700

    Static

--- instyle/settings.py
+++ instyle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w_xn#9d9gj9i8vg4u9u!7y=--p2(-%!!p!s^f96obp!#p)hm09'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['in-style.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'in-style.herokuapp.com']
 
 
 # Application definition
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # For serving static files.
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'instyle.urls'
@@ -132,4 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
