57e9c8a7a904eaf42c04033abca5107de35e9a1b
Darren Tian <darrentianyy@gmail.com>
2018-10-17 22:25:29 -0700

    Skeleton Style Page

--- style/models.py
+++ style/models.py
@@ -2,28 +2,35 @@ from django.db import models
 
 """
 style {
-	styleImageUrl: 'instyle.com/1.png'
-	styleImageAnnotation: [
+	style_image_url: 'instyle.com/1.png'
+	style_image_annotations: [
 		{
 			url: 'instyle.com/product/1'
 			image_url : 'instyle.com/product/1.png'
-			coordinates : {
-				x : 5px
-				y : 10px
-			}
+			coor_x : 40.0
+			coor_y : 35.1
 		},
 		{
 			url: 'instyle.com/product/2'
 			image_url : 'instyle.com/product/2.png'
-			coordinates : {
-				x : 50px
-				y : 100px
-			}
+			coor_x : 70.0
+			coor_y : 69.1
 		}
 	]
 }
 """
 class Style(models.Model):
-	style_image_url = models.CharField(max_length=200)
+	style_image_url = models.URLField(max_length=200)
+
+	def __str__(self):
+		return "%s" % self.style_image_url
+
 	
-		
+class StyleImageAnnotation(models.Model):
+	style = models.ForeignKey(Style, related_name='style_image_annotations', on_delete=models.CASCADE)
+	url = models.URLField(max_length=200)
+	image_url = models.URLField(max_length=200)
+	# Coordinates from left by percentage vs. image.
+	coor_x = models.FloatField()
+	# Coordinates from top by percentage vs. image.
+	coor_y = models.FloatField()
--- style/serializers.py
+++ style/serializers.py
@@ -1,8 +1,14 @@
-from style.models import Style
+from style.models import Style, StyleImageAnnotation
 from rest_framework import serializers
 
+class StyleImageAnnotationSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = StyleImageAnnotation
+		fields = ['url', 'image_url', 'coor_x', 'coor_y']
 
-class StyleSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Style
-        fields = ['url', 'style_image_url']
+class StyleSerializer(serializers.ModelSerializer):
+	style_image_annotations = StyleImageAnnotationSerializer(many=True)
+
+	class Meta:
+		model = Style
+		fields = ['url', 'style_image_url', 'style_image_annotations']
