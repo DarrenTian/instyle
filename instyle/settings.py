9c71fde642fa79c27e08f0d9068445374f4f32d4
Yayunt Home <darrentianyy@gmail.com>
2019-04-28 14:41:34 -0700

    Revert "Revert "Fix style page""
    This reverts commit 039b825afe088d57e8fc89588a07f0534df85c33.

--- instyle/settings.py
+++ instyle/settings.py
@@ -209,6 +209,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/assets'),
     os.path.join(BASE_DIR, 'staticfiles'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
