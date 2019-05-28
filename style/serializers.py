90e4081cbf32d4da69f4237b761ff25576c77c51
FooBar <infiniteauc@gmail.com>
2019-05-27 17:36:39 -0700

    First of many model changes.

--- style/serializers.py
+++ style/serializers.py
@@ -1,25 +1,28 @@
-from style.models import Style, StyleImageAnnotation
+from style.models import Style, StyleImage, StyleImageAnnotation
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class StyleImageAnnotationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StyleImageAnnotation
-		fields = ['url', 'image_url', 'coor_x', 'coor_y']
+		fields = ['style_image', 'product_url', 'product_image_url', 'coor_x', 'coor_y']
+
+class StyleImageSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = StyleImage
+		fields = ['style', 'image']
 
 class StyleSerializer(serializers.ModelSerializer):
-	publisher = serializers.CharField(source='publisher.username')
-	style_image_annotations = StyleImageAnnotationSerializer(many=True)
+	#publisher = serializers.CharField(source='publisher.username')
 
 	class Meta:
 		model = Style
 		fields = [
-			'url', 
 			'title',
 			'credit',
 			'credit_link',
 			'description',
 			'publisher',
-			'publish_date',
-			'style_image_url', 
-			'style_image_annotations',
+			'publish_date', 
 		]
+
