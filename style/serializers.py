17ab76018cf45e5a5dcb0c53d59fdc1e126ffd0c
FooBar <infiniteauc@gmail.com>
2019-05-31 23:13:44 -0700

    First shot at nested style list and style view API
    First shot at nested style list and style view API

--- style/serializers.py
+++ style/serializers.py
@@ -1,20 +1,48 @@
 from style.models import Style, StyleImage, StyleImageAnnotation
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from instyle import settings
+from django.contrib.sites.shortcuts import get_current_site
 
 class StyleImageAnnotationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StyleImageAnnotation
 		fields = ['style_image', 'product_url', 'product_image_url', 'coor_x', 'coor_y']
 
+class StyleImageAnnotationReadSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = StyleImageAnnotation
+		fields = ['product_url', 'product_image_url', 'coor_x', 'coor_y']
+
 class StyleImageSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StyleImage
 		fields = ['style', 'image']
 
-class StyleSerializer(serializers.ModelSerializer):
-	#publisher = serializers.CharField(source='publisher.username')
+class StyleImageReadSerializer(StyleImageSerializer):
+    image = serializers.SerializerMethodField('get_image_url')
+    def get_image_url(self, obj):
+    	return ''.join(['http://', get_current_site(self.context['request']).domain, obj.image.url])
 
+    class Meta:
+		model = StyleImage
+		fields = ['image']
+
+class StyleImageReadSerializerNested(serializers.Serializer):
+    image = serializers.SerializerMethodField('get_image_url')
+    image_annotations = serializers.SerializerMethodField('get_image_annotation')
+    def get_image_url(self, obj):
+    	return ''.join(['http://', get_current_site(self.context['request']).domain, obj.image.url])
+
+    def get_image_annotation(self, obj):
+    	sia_qs = StyleImageAnnotation.objects.filter(style_image = obj)
+    	sia_s = StyleImageAnnotationReadSerializer(sia_qs, many=True) 
+    	return sia_s.data
+
+    class Meta:
+		fields = ['image', 'image_annotations']
+
+class StyleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Style
 		fields = [
@@ -23,6 +51,26 @@ class StyleSerializer(serializers.ModelSerializer):
 			'credit_link',
 			'description',
 			'publisher',
-			'publish_date', 
+			'publish_date',	
 		]
+
+	def create(self, validated_data):
+		return Style.objects.create(**validated_data)
+
+class StyleReadSerializerNested(serializers.Serializer):
+	style = serializers.SerializerMethodField('get_style_description')
+	images = serializers.SerializerMethodField('get_images_description')
+
+	def get_style_description(self, obj):
+		return StyleSerializer(obj).data
+
+	def get_images_description(self, obj):
+		style_image_queryset = StyleImage.objects.filter(style = obj)
+		style_image_serializer = StyleImageReadSerializerNested(style_image_queryset, many=True, context={'request': self.context['request']})
+		return style_image_serializer.data
+		
+	class Meta:
+		fields = ['style', 'images']
+
+
 
--- style/views.py
+++ style/views.py
@@ -1,17 +1,35 @@
 from django.core.files.storage import FileSystemStorage
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
 from style.models import Style, StyleImage, StyleImageAnnotation
-from style.serializers import StyleSerializer, StyleImageSerializer, StyleImageAnnotationSerializer
+from style.serializers import StyleSerializer,StyleReadSerializerNested, StyleImageSerializer, StyleImageAnnotationSerializer
+from rest_framework.response import Response
+import logging
 
 class StyleViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Style.objects.all().order_by('id')
     serializer_class = StyleSerializer
+
+    def list(self, request):
+    	return Response(StyleReadSerializerNested(Style.objects.all(), many=True, context={'request': request}).data)
+
+    def retrieve(self, request, pk=None):
+    	style_queryset = Style.objects.all()
+    	style = get_object_or_404(style_queryset, pk=pk)
+    	style_serializer = StyleReadSerializerNested(style, context={'request': request})
+    	return Response(style_serializer.data)
+
+    def update(self, request, pk=None):
+    	pass
+
+    def partial_update(self, request, pk=None):
+    	pass
+
+    def destroy(self, request, pk=None):
+    	pass
+
 
 class StyleImageViewSet(viewsets.ModelViewSet):
 	queryset = StyleImage.objects.all().order_by('id')
