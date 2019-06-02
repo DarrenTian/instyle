67faa91884444c8483ea734e7a001fb71836fa5c
FooBar <infiniteauc@gmail.com>
2019-06-01 21:58:44 -0700

    Revert "Merge branch 'foobar/dev' into development"
    This reverts commit 8ece16e5d46dbc3ce9ca2f097d92ebf4b453f18d, reversing
    changes made to b8d43ac6257e8017dabb200b1044c2b6a88b96ce.

--- style/serializers.py
+++ style/serializers.py
@@ -1,32 +1,25 @@
-from style.models import Style, StyleImage, StyleImageAnnotation
+from style.models import Style, StyleImageAnnotation
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from instyle import settings
-from django.contrib.sites.shortcuts import get_current_site
 
 class StyleImageAnnotationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StyleImageAnnotation
-		fields = ['style_image', 'url', 'image_url', 'coor_x', 'coor_y', 'title', 'price']
-
-class StyleImageSerializer(serializers.ModelSerializer):
-	style_image_annotation = StyleImageAnnotationSerializer(read_only=True, many=True)
-	class Meta:
-		model = StyleImage
-		fields = ['style', 'image', 'style_image_annotation']
+		fields = ['url', 'image_url', 'coor_x', 'coor_y', 'title', 'price']
 
 class StyleSerializer(serializers.ModelSerializer):
-	style_image = StyleImageSerializer(read_only=True, many=True)
+	publisher = serializers.CharField(source='publisher.username')
+	style_image_annotations = StyleImageAnnotationSerializer(many=True)
 
 	class Meta:
 		model = Style
 		fields = [
+			'url', 
 			'title',
 			'credit',
 			'credit_link',
 			'description',
-      'tags',
 			'publisher',
 			'publish_date',
-			'style_image'	
+			'style_image_url', 
+			'style_image_annotations',
 		]
--- style/views.py
+++ style/views.py
@@ -1,23 +1,37 @@
 from django.core.files.storage import FileSystemStorage
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
-from style.models import Style, StyleImage, StyleImageAnnotation
-from style.serializers import StyleSerializer,StyleImageSerializer, StyleImageAnnotationSerializer
-from rest_framework.response import Response
-import logging
+from style.models import Style
+from style.serializers import StyleSerializer
 
 class StyleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = Style.objects.all().order_by('id')
     serializer_class = StyleSerializer
 
+def welcome(request):
+	return render(request, 'style/welcome.html', {})
 
-class StyleImageViewSet(viewsets.ModelViewSet):
-	queryset = StyleImage.objects.all().order_by('id')
-	serializer_class = StyleImageSerializer
+def new_style(request):
+	if request.method == 'POST' and request.FILES['style_image']:
+		# style_image = request.FILES['style_image']
+		# fs = FileSystemStorage()
+		# filename = fs.save(style_image.name, style_image)
+		# uploaded_file_url = fs.url(filename)
 
-class StyleImageAnnotationViewSet(viewsets.ModelViewSet):
-	queryset = StyleImageAnnotation.objects.all().order_by('id')
-	serializer_class = StyleImageAnnotationSerializer
-		
+		# new_style = Style()
+		# new_style.style_image_url = uploaded_file_url
+		# new_style.save()
+		# image_id = str(new_style.pk)
+		image_id = '13'
+		return redirect('/style/'+image_id+'/edit')
+	return render(request, 'style/new.html')
+
+def edit_style(request, style_id):
+	style = Style.objects.get(id=style_id)
+	context = { 'style': style }
+	return render(request, 'style/edit.html', context)
