4f94203cf8e6ca0cb898944b36af861e84f1fa23
Yayunt Home <darrentianyy@gmail.com>
2019-06-30 22:51:30 -0700

    Console page mobile version

--- frontend/src/components/Console.js
+++ frontend/src/components/Console.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { withRouter } from 'react-router';
 
+import MediaQuery from 'react-responsive';
+
 class Console extends React.Component {
 	constructor(props) {
 		super(props);
@@ -10,44 +12,60 @@ class Console extends React.Component {
 		const consoleWrapperStyle = {
 		}
 		const consoleStyle = {
-			width: "769px",
-			padding: "1rem 1.5rem",
-			margin: "1rem auto",
+			maxWidth: "769px",
+			padding: "0rem 0.5rem",
+			margin: "0rem auto",
 			justifyContent: "flex-start",
 		}
+		const newLookStyle = {
+			margin: "0.5rem",
+		}
 		const looksStyle = {
-			padding: "1rem 0.5rem",
+			padding: "0rem 0.5rem",
 			display: "flex",
 			flexDirection: "row",
 			flexWrap: "wrap",
 			boxShadow: "0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1)",
-			marginTop: "15px",
+			marginTop: "0",
 			borderRadius: "5px",
-			//justifyContent: "space-between",
+			//justifyContent: "space-around",
 		}
 		const lookStyle = {
-			width: "235px",
 			padding: "10px",
 			borderRadius: "5px",
+			//flex: "0 0 25%",
 		}
 		const previewImageStyle = {
 			borderRadius: "5px",
 		}
-		const looks = [1,2,3]
+		const looks = [1,1,1,1,1,1]
 		return (
 				<div className="console" style={consoleStyle} >
-						<button className="button">
+				    <MediaQuery query="(min-width: 769px)">
+				    	<button className="button is-hidden-mobile" style={newLookStyle}>
 							Create New Look
 						</button>
-						<div className="" style={looksStyle}>
+						<div className="columns is-mobile" style={looksStyle}>
 							{looks.map((value, index)=>{
 								return (
-									<div style={lookStyle} key={index}>
+									<div className="column is-one-quarter" style={lookStyle} key={index}>
 										<img style={previewImageStyle} src="./media/addict.attitude/1.jpg"></img>
 									</div>
 								)
 							})}
 						</div>
+                    </MediaQuery>
+                    <MediaQuery query="(max-width: 769px)">
+                    	<div className="columns is-mobile is-shadowless" style={looksStyle}>
+							{looks.map((value, index)=>{
+								return (
+									<div className="column is-half" style={lookStyle} key={index}>
+										<img style={previewImageStyle} src="./media/addict.attitude/1.jpg"></img>
+									</div>
+								)
+							})}
+						</div>
+                    </MediaQuery>
 				</div>
 		)
 	}
