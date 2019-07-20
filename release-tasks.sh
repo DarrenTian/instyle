b082f173e341f7eb995f77457febb8e25230af06
Yayunt Home <darrentianyy@gmail.com>
2019-07-20 10:57:43 -0700

    Added heroku buildpack instead

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,3 @@
-npm install --dev
-webpack --mode production --env.PROD_ENV=PROD ./frontend/src/index.js --output ./staticfiles/main.js
+# Detect and migrate django db changes
 python manage.py makemigrations
 python manage.py migrate
