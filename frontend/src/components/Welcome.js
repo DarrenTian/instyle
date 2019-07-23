0954efd5274bf7270009989e699955bcb405a3d7
Yayunt Home <darrentianyy@gmail.com>
2019-07-22 22:35:41 -0700

    use s3 path for home page

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -3,6 +3,11 @@ import Invite from "./Invite";
 import React from "react";
 import PropTypes from "prop-types";
 
+const getImageSrc = (imagePath) => {
+    const imagePrefix = 'https://eastyler-static.s3.amazonaws.com/media/';
+    return imagePrefix + imagePath;
+}
+
 const Welcome = () => {
     return (
         <div>
@@ -10,12 +15,12 @@ const Welcome = () => {
                 <div className="overlay"></div>
                 <section className="section tiles-section">
                         <div className="tiles-vertical-columns">
-                            <ImageTile source={"./media/fangzheng/1.jpg"}/>
-                            <ImageTile source={"./media/addict.attitude/1.jpg"}/>
-                            <ImageTile source={"./media/addict.attitude/2.jpg"}/>    
-                            <ImageTile source={"./media/fangzheng/2.jpg"}/>
-                            <ImageTile source={"./media/addict.attitude/7.jpg"}/>
-                            <ImageTile source={"./media/addict.attitude/8.jpg"}/>
+                            <ImageTile source={getImageSrc('fangzheng/1.jpg')}/>
+                            <ImageTile source={getImageSrc('addict.attitude/1.jpg')}/>
+                            <ImageTile source={getImageSrc('addict.attitude/2.jpg')}/>    
+                            <ImageTile source={getImageSrc('fangzheng/2.jpg')}/>
+                            <ImageTile source={getImageSrc('addict.attitude/7.jpg')}/>
+                            <ImageTile source={getImageSrc('addict.attitude/8.jpg')}/>
                         </div>
                 </section>
             </div>
